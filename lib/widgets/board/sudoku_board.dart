39f4c6a9cea9f9f0355173446a90000c46e576af
NaZe0320 <rwt1226@naver.com>
2025-02-05 12:18:22 +0900

    [feat] 스도쿠 보드 - 여러 기종 대응 구현

--- lib/widgets/board/sudoku_board.dart
+++ lib/widgets/board/sudoku_board.dart
@@ -8,23 +8,36 @@ class SudokuBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
+    final maxHeight = screenHeight * 0.6;
+    final screenWidth = MediaQuery.of(context).size.width;
+    final boardSize = (maxHeight < screenWidth) ? maxHeight : screenWidth;
+
     return Consumer<GameProvider>(
       builder: (context, gameProvider, child) {
-        return AspectRatio(
-          aspectRatio: 1, // 정사각형 보드
-          child: Container(
-            decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 2)),
-            child: GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 9),
-              itemCount: 81,
-              itemBuilder: (context, index) {
-                final row = index ~/ 9;
-                final col = index % 9;
-                final cell = gameProvider.currentBoard.getCell(row, col);
+        return Center(
+          child: SizedBox(
+            height: maxHeight,
+            child: Center(
+              child: SizedBox(
+                width: boardSize,
+                height: boardSize,
+                child: Container(
+                  decoration: BoxDecoration(border: Border.all(color: Colors.black, width: 2)),
+                  child: GridView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 9),
+                    itemCount: 81,
+                    itemBuilder: (context, index) {
+                      final row = index ~/ 9;
+                      final col = index % 9;
+                      final cell = gameProvider.currentBoard.getCell(row, col);
 
-                return BoardCell(key: Key('cell_$row$col'), row: row, col: col, cell: cell);
-              },
+                      return BoardCell(key: Key('cell_$row$col'), row: row, col: col, cell: cell);
+                    },
+                  ),
+                ),
+              ),
             ),
           ),
         );
